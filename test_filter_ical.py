1afc159742fdabd2345b807b8844a04e27676d3d
mick <mick.taige@gmail.com>
2024-07-07 10:08:18 +1200

    Add empty test cases.

---
+++ test_filter_ical.py
@@ -0,0 +1,34 @@
+from click.testing import CliRunner
+from filterIcal import filter_ical
+
+def test_date_range_gt():
+    """
+    TODO
+    """
+    # runner = CliRunner()
+    # result = runner.invoke(filter_ical, [])
+    # print("res: ",result.output)
+
+def test_date_range_lt():
+    """
+    TODO
+    """
+    print("TODO")
+
+def test_date_range_between():
+    """
+    TODO
+    """
+    print("TODO")
+
+def test_no_organiser():
+    """
+    TODO
+    """
+    print("TODO")
+
+def test_organiser():
+    """
+    TODO
+    """
+    print("TODO")
