ab28126a20e6e39a0b5c33629b967e10ee8c0c4a
mick <mick.taige@gmail.com>
2024-07-07 16:06:41 +1200

    Add first test case implementation.

--- test_filter_ical.py
+++ test_filter_ical.py
@@ -1,13 +1,42 @@
 from click.testing import CliRunner
-from filterIcal import filter_ical
+from filter_ical import filter_ical
+from icalendar import Calendar
+
+#############################
+# From test ical file
+#############################
+
+# BEGIN:VEVENT
+# DTSTART:20200918T143000Z
+# DTEND:20200918T153000Z
+# ORGANIZER;CN="Organizer Email";EMAIL="email@email.com":mailto:email@email.com
+# SUMMARY:Test Event 1
+# END:VEVENT
+
+# BEGIN:VEVENT
+# DTSTART:20200919T143000Z
+# DTEND:20200919T153000Z
+# ORGANIZER;CN="Organizer Email";EMAIL="email@email.com":mailto:email@email.com
+# SUMMARY:Test Event 2
+# END:VEVENT
+
+# BEGIN:VEVENT
+# DTSTART:20200920T143000Z
+# DTEND:20200920T153000Z
+# ORGANIZER;CN="Organizer Email";EMAIL="email@email.com":mailto:email@email.com
+# SUMMARY:Test Event 3
+# END:VEVENT
 
 def test_date_range_gt():
     """
     TODO
     """
-    # runner = CliRunner()
-    # result = runner.invoke(filter_ical, [])
-    # print("res: ",result.output)
+    runner = CliRunner()
+    result = runner.invoke(filter_ical, ["testCalendar.ics","-f","1st Jan 2019 7pm NZST"])
+
+    calendar=Calendar.from_ical(result.output.strip())
+
+    assert len(calendar.walk('VEVENT')) == 3
 
 def test_date_range_lt():
     """
